f516030560ca4d6383bc564363f1d6a133d19d35
geraldgartner <Gerald>
2018-08-02 16:59:39 +0200

    update

--- scripts/01-preprocessing.R
+++ scripts/01-preprocessing.R
@@ -22,21 +22,56 @@
 # saveRDS(data,file="output/ignore/data.Rda")
 data <- readRDS("output/ignore/data.Rda")
 
+data$long <- data$latlong$lon
+data$lat <- data$latlong$lat
+data$geocodedadress <- data$latlong$address
+
+data <- data %>%
+  separate(Beschäftigte, into=c("beschaeftigte_jahr", "beschaeftigte"), sep=":") %>%
+  select(-latlong)
+
+data$beschaeftigte <- trimws(data$beschaeftigte) 
+data$beschaeftigte[is.na(data$beschaeftigte)] <- "n.v."
+data$beschaeftigte[data$beschaeftigte == "kei"] <- 0
+data$beschaeftigte[data$beschaeftigte == "keine"] <- 0
+
+write.csv(data, "output/data_companies_geocoded.csv")
+
 #Gathern
 data <- data %>%
-  select(-sparten,everything()) %>%
   separate(sparten, into=c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"), sep=",") %>%
+  select(-c(a:j),everything()) %>%
   gather(sparten, typ, a:j) %>%
   filter(!is.na(typ) & typ !="")
 
-  data$typ <- trimws(data$typ)  
-  
+
+data$typ <- trimws(data$typ)  
+
+#Beschäftigte in eigener Spalte
+
+
+    
 #exporte reinladen, aber nur ab 2004, weil sonst keine kategorisierung vorliegt
 exports <- read_xlsx("input/exporte_eu_pivot.xlsx", sheet="roh")%>%
   mutate(jahr = as.numeric(year))%>%
-  filter(year>2003 & rating !="Miscellaneous")%>%
+  filter(year>2003 & rating !="Miscellaneous" & name_destination_country !="Federal Republic of Yugoslavia")%>%
   select(-X__1)
 
 #wk dokumentation hinzufügen
 exports <- left_join(exports, dok, by=c("rating"="mlcat"))
+
+#centroids laden
+centroid <- read_delim("input/country_centroids_all.csv", 
+                                         "\t", escape_double = FALSE, trim_ws = TRUE)%>%
+  select(ISO3136, SHORT_NAME, LAT, LONG)
+
+#centroid hinzufügen
+exports <- left_join(exports, centroid, by=c("iso_source_country"="ISO3136"))%>%
+                  rename(lat_source_country = LAT, 
+                  long_source_country = LONG) 
+
+exports <- left_join(exports, centroid, by=c("iso_destination_country"="ISO3136"))%>%
+                   rename(lat_destination_country = LAT, 
+                    long_destination_country = LONG)
+  
 
